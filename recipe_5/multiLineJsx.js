981923a39a2e7ba24570ef4b19518832de58359b
Jeff Simonitto <jsimonitto@gmail.com>
2020-08-23 15:51:15 -0400

    recipe 5

---
+++ recipe_5/multiLineJsx.js
@@ -0,0 +1,8 @@
+// Multiline JSX must be wrapped in parentheses
+
+// ... return from Function or render method from class-based components
+return (
+  <div className="App">
+    <p>Hello World!</p>
+  </div>
+);
